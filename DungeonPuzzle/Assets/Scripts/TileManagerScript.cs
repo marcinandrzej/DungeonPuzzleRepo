e3414c48032ec15572ae8de64c8d7e291c767a49
marcinandrzej <andrews1993@wp.pl>
2019-03-24 11:02:32 +0100

    v 0.3
    Level setting script and tile script

---
+++ DungeonPuzzle/Assets/Scripts/TileManagerScript.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileManagerScript : MonoBehaviour
+{
+    private const int X_OFFSET = 10;
+    private const int Y_OFFSET = -10;
+    private const float SPEED = 0.5f;
+
+    public static TileManagerScript instance;
+    public GameObject[] tilePrefabs;
+    private GameObject[,] tiles;
+    public GameObject tilesParent;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if(instance != this)
+        {
+            Destroy(instance.gameObject);
+            instance = this;
+        }
+    }
+
+	// Use this for initialization
+	void Start ()
+    {
+        int[,] map = new int[5, 5];
+        for (int x = 0; x < map.GetLength(0); x++)
+        {
+            for (int y = 0; y < map.GetLength(1); y++)
+            {
+                map[x, y] = 1;
+            }
+        }
+        for (int x = 1; x < map.GetLength(0) - 1; x++)
+        {
+            for (int y = 1; y < map.GetLength(1) - 1; y++)
+            {
+                map[x, y] = 0;
+            }
+        }
+        map[2, 0] = 3;
+        map[2, 4] = 2;
+        map[1, 1] = 5;
+        map[1, 2] = 4;
+        map[2, 2] = 5;
+        map[3, 2] = 6;
+        map[3, 3] = 7;
+
+        SetUpTiles(tilesParent.transform, map);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void SetUpTiles(Transform parent, int[,] tileMap)
+    {
+        tiles = new GameObject[tileMap.GetLength(0), tileMap.GetLength(1)];
+        for (int x = 0; x < tiles.GetLength(0); x++)
+        {
+            for (int y = 0; y < tiles.GetLength(1); y++)
+            {
+                if (tileMap[x, y] != 0)
+                {
+                    tiles[x, y] = Instantiate(tilePrefabs[tileMap[x, y] - 1], tilesParent.transform, false);
+                    tiles[x, y].GetComponent<TileScript>().SetPosition(x, y, CalculatePosition(x, y, 0));
+                }
+            }
+        }
+    }
+
+    public Vector3 CalculatePosition(int x, int y, float hight)
+    {
+        Vector3 calculatedPos = new Vector3((x - 1) * X_OFFSET, hight, (y - 1) * Y_OFFSET);
+        return calculatedPos;
+    }
+}
--- DungeonPuzzle/Assets/Scripts/TileScript.cs
+++ DungeonPuzzle/Assets/Scripts/TileScript.cs
@@ -2,15 +2,63 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TileScript : MonoBehaviour {
+public class TileScript : MonoBehaviour
+{
+    private int xIndex;
+    private int yIndex;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public bool passable;
+
+    public int xExit1;
+    public int yExit1;
+
+    public int xExit2;
+    public int yExit2;
+
+    public int XIndex
+    {
+        get
+        {
+            return xIndex;
+        }
+
+        set
+        {
+            xIndex = value;
+        }
+    }
+
+    public int YIndex
+    {
+        get
+        {
+            return yIndex;
+        }
+
+        set
+        {
+            yIndex = value;
+        }
+    }
+
+    public void SetPosition(int _xIndex, int _yIndex, Vector3 pos)
+    {
+        XIndex = _xIndex;
+        YIndex = _yIndex;
+        transform.position = pos;
+    }
+
+    public IEnumerator Move(Vector3 destination, float speed)
+    {/*
+        float destinationX = (XIndex - 1) * X_OFFSET;
+        float destinationY = (YIndex - 1) * Y_OFFSET;
+        Vector3 destination = new Vector3(destinationX, transform.position.y, destinationY);
+        */
+        while (!transform.localPosition.Equals(destination))
+        {
+            transform.localPosition = Vector3.MoveTowards(transform.localPosition, destination, speed);
+            yield return new WaitForEndOfFrame();
+        }
+        yield return new WaitForEndOfFrame();
+    }
 }
