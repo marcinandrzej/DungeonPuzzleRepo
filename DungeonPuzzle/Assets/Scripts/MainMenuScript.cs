fdcc3aefab1d4cc35c1da4198ed0ff1658bb8a20
marcinandrzej <andrews1993@wp.pl>
2019-04-04 15:32:41 +0200

    v 0. 97
    Audio mixer, menu scripting

---
+++ DungeonPuzzle/Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+public class MainMenuScript : MonoBehaviour
+{
+    public GameObject panelOption;
+    public GameObject panelLoading;
+
+    public Sprite soundEnabled;
+    public Sprite soundDisabled;
+    public Sprite musicEnabled;
+    public Sprite musicDisabled;
+
+    public Image LoadingBar;
+    public Text loadingText;
+
+    public AudioMixer audioMixer;
+
+    public Slider soundSlider;
+    public Slider musicSlider;
+
+    public Image soundButton;
+    public Image musicButton;
+
+    private bool soundOn;
+    private bool musicOn;
+
+	// Use this for initialization
+	void Start ()
+    {
+        soundOn = true;
+        musicOn = true;
+        soundSlider.onValueChanged.AddListener(delegate { SetSoundVolume(); });
+        musicSlider.onValueChanged.AddListener(delegate { SetMusicVolume(); });	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ShowOptions()
+    {
+        panelOption.SetActive(!panelOption.activeSelf);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    public void LoadLevel(string name)
+    {
+        panelLoading.SetActive(true);
+        StartCoroutine(loadCoroutine(name));
+    }
+
+    private IEnumerator loadCoroutine(string name)
+    {
+        AsyncOperation loading = SceneManager.LoadSceneAsync(name);
+        while (!loading.isDone)
+        {
+            loadingText.color = new Color(loadingText.color.r, loadingText.color.g, loadingText.color.b, Mathf.PingPong(Time.time, 1));
+            LoadingBar.fillAmount = loading.progress;
+            yield return new WaitForEndOfFrame();
+        }
+        yield return new WaitForEndOfFrame();
+    }
+
+    public void SoundButtonClick()
+    {
+        soundOn = !soundOn;
+        if (soundOn)
+        {
+            soundButton.sprite = soundEnabled;
+        }
+        else
+        {
+            soundButton.sprite = soundDisabled;
+        } 
+        SetSoundVolume();
+    }
+
+    public void MusicButtonClick()
+    {
+        musicOn = !musicOn;
+        if (musicOn)
+        {
+            musicButton.sprite = musicEnabled;
+        }
+        else
+        {
+            musicButton.sprite = musicDisabled;
+        }
+        SetMusicVolume();
+    }
+
+    public void SetMusicVolume()
+    {
+        if (musicOn)
+        {
+            audioMixer.SetFloat("MusicVolume", Mathf.Log10(musicSlider.value) * 20);
+        }
+        else
+        {
+            audioMixer.SetFloat("MusicVolume", -80);
+        }
+    }
+
+    public void SetSoundVolume()
+    {
+        if (soundOn)
+        {
+            audioMixer.SetFloat("SoundVolume", Mathf.Log10(soundSlider.value) * 20);
+        }
+        else
+        {
+            audioMixer.SetFloat("SoundVolume", -80);
+        }
+    }
+}
